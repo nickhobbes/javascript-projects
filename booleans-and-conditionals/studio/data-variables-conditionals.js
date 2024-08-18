b0e3efe63b99af60b3f7dbb0db6df4c95e6faaf2
Nicholas Cerame <nickcerame@Nicholass-MacBook-Air.local>
2024-08-18 06:18:04 -0500

    Studio: Shuttle Launch is completed and working properly.

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,15 +1,75 @@
 // Initialize Variables below
 
-// add logic below to verify total number of astronauts for shuttle launch does not exceed 7
+let date = "Monday 2019-03-18";
+let time = "10:05:34 AM";
 
-// add logic below to verify all astronauts are ready
+let astronautCount = 7;
+let astronautStatus = "ready";
 
-// add logic below to verify the total mass does not exceed the maximum limit of 850000
+let averageAstronautMassKg = 80.7;
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let maximumMassLimit = 850000;
 
-// add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
 
-// add logic below to verify the fuel level is at 100%
+let fuelLevel = 100;
+let weatherStatus = "clear";
+let preparedForLiftoff = true;
 
-// add logic below to verify the weather status is clear
+if (astronautCount > 7) {
+    console.log("Too many astronauts");
+    preparedForLiftoff = false;
+}
+
+if (astronautStatus !== "ready") {
+    console.log("Astronauts are not ready");
+    preparedForLiftoff = false;
+}
+
+if (totalMassKg > maximumMassLimit) {
+    console.log("Total mass exceeds maximum mass limit.");
+    preparedForLiftoff = false;
+}
+
+if (fuelTempCelsius < -300 || fuelTempCelsius > -150) {
+    console.log("Fuel temperature is not within the limits.");
+    preparedForLiftoff = false;
+}
+
+if (fuelLevel !== 100) {
+    console.log("Fuel level is not at 100%.");
+    preparedForLiftoff = false;
+}
+
+if (weatherStatus !== "clear") {
+    console.log("Weather is not clear for liftoff.");
+    preparedForLiftoff = false;
+}
 
 // Verify shuttle launch can proceed based on above conditions
+
+if(preparedForLiftoff) {
+    console.log("All systems are a go! Initiating space shuttle launch sequence.");
+    console.log("---------------------------------------------------------------");
+
+    console.log("Date:", date);
+    console.log("Time:", time);
+    console.log("Astronaut Count:", astronautCount);
+    console.log("Crew Mass:", crewMassKg, "kg");
+    console.log("Fuel Mass:", fuelMassKg, "kg");
+    console.log("Shuttle Mass:", shuttleMassKg, "kg");
+    console.log("Total Mass:", totalMassKg, "kg");
+    console.log("Fuel Temperature:", fuelTempCelsius, "°C");
+    console.log("Weather Status:", weatherStatus);
+
+    console.log("---------------------------------------------------------------");
+    console.log("Have a safe trip astronauts!");
+} else {
+    console.log("---------------------------------------------------------------");
+    console.log("Not prepared for liftoff! Shutting down all launch operations!")
+}
