66be3c156a2fc9fc56bb60d2371ab0c40715e871
Nicholas Cerame <nickcerame@Nicholass-MacBook-Air.local>
2024-08-18 07:08:51 -0500

    Chapter 6 exercises complete and working properly.

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -4,7 +4,26 @@ let num = 1001;
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
+console.log(String(num).length);
+
+num = 123.45;
 
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
 
+console.log(String(num).length - 1);
+
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
+
+if (String(num).includes('.')) {
+    console.log(String(num).length - 1);
+} else {
+    console.log(String(num).length);
+}
+
+num = 1233;
+
+if (String(num).includes('.')) {
+    console.log(String(num).length - 1);
+} else {
+    console.log(String(num).length);
+}
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -4,14 +4,28 @@ let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
 
+console.log(language.slice(0, 1) + language.slice(4,5));
+
 //2. Without using slice(), use method chaining to accomplish the same thing.
+
+let initials = language.charAt(0).concat(language.charAt(4));
+console.log(initials);
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
 
+console.log(`The abbreviation for ${language} is ${initials}.`);
+
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
+
+console.log(language.toUpperCase().concat(initials.toLowerCase()));
 
 //Part Three section Two
 
 //1. Use the string methods you know to print 'Title Case' from the string 'title case'.
 
 let notTitleCase = 'title case';
+notTitleCase = notTitleCase.replace(notTitleCase.charAt(0), notTitleCase.charAt(0).toUpperCase());
+let spaceIndex = notTitleCase.indexOf(" ") + 1;
+notTitleCase = notTitleCase.replace(notTitleCase.charAt(spaceIndex), notTitleCase.charAt(spaceIndex).toUpperCase());
+
+console.log(notTitleCase);
